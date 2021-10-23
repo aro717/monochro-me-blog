ea818ae54e443233beb73cdea5c9849dbe0ac90c
aro717 <monochro_me_aro@yahoo.co.jp>
2021-10-23 19:38:20 +0900

    Initial Commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -35,7 +35,7 @@ DEBUG = True
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['aro.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
